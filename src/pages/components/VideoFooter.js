6102b1d149e9649b33d7c70ada2f053e0b2dada9
Giliardi <giliardi.oliveira@gmail.com>
2023-04-02 15:18:17 -0300

    Aula 2

---
+++ src/pages/components/VideoFooter.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./videoFooter.css";
+
+export default function VideoFooter() {
+  return (
+    <div className="videoFooter">
+      <div className="videoFooter__text">
+        <h3>@Giliardi Ebac</h3>
+        <p>Descrição do Video</p>
+        <p>Icone</p>
+        <p>Titulo da musica</p>
+      </div>
+    </div>
+  );
+}
